9988812fca1bb15cac090c51bee920ebe7c69a07
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2026-05-19 09:52:04 +0100

    deletado arquivos de react não mais necessários

--- 01_PostgreSQL/CaseStudy_DataSubmission/Presentation/app/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import AnacLoad4Slides from './AnacLoad4Slides.jsx'
-// import AnacLoadMethodsPresentation from './AnacLoadMethodsPresentation.jsx' // versão original 9 slides
-
-function App() {
-  return <AnacLoad4Slides />
-}
-
-export default App
